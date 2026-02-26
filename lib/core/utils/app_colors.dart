a201c62b139b149d8896aaf794a2a1911a301769
FarahSa1 <farahsalemm1@gmail.com>
2026-02-26 14:28:41 +0200

    add primary color palette and text style files

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryBlue = Color(0XFF006AFF);
+  static const Color secondaryRed = Color(0XFFFF2727);
+  static const Color black = Color(0XFF1A1919);
+  static const Color gray02Dark = Color(0XFF525256);
+  static const Color gray02Light = Color(0XFF656575);
+  static const Color gray03 = Color(0XFFA3A3A3);
+  static const Color green = Color(0XFF52C93F);
+  static const Color gray04 = Color(0XFFF8F7F1);
+}
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,137 @@
+import 'package:car_rental_dashboard/core/utils/size_config.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static TextStyle styleLight14(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  static TextStyle styleLight16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  static TextStyle styleRegular12(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular14(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular24(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular32(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleMedium14(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium18(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium20(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold32(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleBold14(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleBold26(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 26),
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleBold28(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 28),
+      fontWeight: FontWeight.w700,
+    );
+  }
+}
+
+double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < SizeConfig.tablet) {
+    return width / 600;
+  } else if (width < SizeConfig.desktop) {
+    return width / 1000;
+  } else {
+    return width / 1800;
+  }
+}
