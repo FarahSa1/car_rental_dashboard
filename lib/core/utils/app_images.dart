21472c80d1a85ef25211eb308cdb21f164c4cc26
FarahSa1 <farahsalemm1@gmail.com>
2026-02-26 14:55:10 +0200

    add app images

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets{
+  Assets._();
+  
+  /// Assets for imagesAvatarTest1
+  /// assets/images/avatar_test1.svg
+  static const String imagesAvatarTest1 = "assets/images/avatar_test1.svg";
+
+  /// Assets for imagesAvatarTest2
+  /// assets/images/avatar_test2.svg
+  static const String imagesAvatarTest2 = "assets/images/avatar_test2.svg";
+
+  /// Assets for imagesAvatarTest3
+  /// assets/images/avatar_test3.svg
+  static const String imagesAvatarTest3 = "assets/images/avatar_test3.svg";
+
+  /// Assets for imagesBlueStatus
+  /// assets/images/blue_status.svg
+  static const String imagesBlueStatus = "assets/images/blue_status.svg";
+
+  /// Assets for imagesGreenStatus
+  /// assets/images/green_status.svg
+  static const String imagesGreenStatus = "assets/images/green_status.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesRedStatus
+  /// assets/images/red_status.svg
+  static const String imagesRedStatus = "assets/images/red_status.svg";
+}
+
