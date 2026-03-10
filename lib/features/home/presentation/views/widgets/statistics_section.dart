4e432d8dd2f2b364bf7624c9f9f3ac815d711afb
FarahSa1 <farahsalemm1@gmail.com>
2026-03-10 23:59:02 +0200

    refactor statistics section widget

--- lib/features/home/presentation/views/widgets/statistics_section.dart
+++ lib/features/home/presentation/views/widgets/statistics_section.dart
@@ -4,14 +4,34 @@ import 'package:car_rental_dashboard/core/utils/size_config.dart';
 import 'package:car_rental_dashboard/features/home/data/models/incom_and_expenses_info_model.dart';
 import 'package:car_rental_dashboard/features/home/presentation/views/widgets/hire_and_cancel_section.dart';
 import 'package:car_rental_dashboard/features/home/presentation/views/widgets/incom_and_expenses_section.dart';
-import 'package:car_rental_dashboard/features/home/presentation/views/widgets/notification_section.dart';
 import 'package:car_rental_dashboard/features/home/presentation/views/widgets/search_box.dart';
-import 'package:car_rental_dashboard/features/home/presentation/views/widgets/search_section.dart';
 import 'package:flutter/material.dart';
 
-class StatisticsSection extends StatelessWidget {
+class StatisticsSection extends StatefulWidget {
   const StatisticsSection({super.key});
 
+  @override
+  State<StatisticsSection> createState() => _StatisticsSectionState();
+}
+
+class _StatisticsSectionState extends State<StatisticsSection> {
+  List<IncomAndExpensesInfoModel> items = [
+     IncomAndExpensesInfoModel(
+              amount: 9460.00,
+              isDown: true,
+              percentage: 1.5,
+              comparisonMount: 9940,
+              lastWeekAmount: 22658.00,
+      ),
+      IncomAndExpensesInfoModel(
+              amount: 5660.00,
+              isDown: false,
+              percentage: 2.5,
+              comparisonMount: 5240,
+              lastWeekAmount: 22658.00,
+            ),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
@@ -39,24 +59,12 @@ class StatisticsSection extends StatelessWidget {
           const SizedBox(height: 16),
           IncomAndExpensesSection(
             sectionTitle: 'Incom',
-            incomAndExpensesInfoModel: IncomAndExpensesInfoModel(
-              amount: 9460.00,
-              isDown: true,
-              percentage: 1.5,
-              comparisonMount: 9940,
-              lastWeekAmount: 22658.00,
-            ),
+            incomAndExpensesInfoModel: items[0]
           ),
           const SizedBox(height: 16),
           IncomAndExpensesSection(
             sectionTitle: 'Expenses',
-            incomAndExpensesInfoModel: IncomAndExpensesInfoModel(
-              amount: 5660.00,
-              isDown: false,
-              percentage: 2.5,
-              comparisonMount: 5240,
-              lastWeekAmount: 22658.00,
-            ),
+            incomAndExpensesInfoModel: items[1]
           ),
           const SizedBox(height: 16),
           HireAndCancelSection(),
